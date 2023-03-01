f5ce890989bd2bcbf13e617d0e3f6a5f79c07e2e
Fredrik Carlsson <me@fc.io>
2023-03-01 14:00:33 +0100

    add fonts

--- app/page.js
+++ app/page.js
@@ -1,15 +1,13 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from './page.module.css'
 import Link from 'next/link'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div>
-        <h1 className={inter.className}>This is my homepage</h1>
+        <h1>This is my homepage</h1>
         <div><Link href="/about">Go to About</Link></div>
         <div><Link href="/portfolio">Go to Portfolio</Link></div>
         <div><Link href="/ping">Go to Ping</Link></div>
